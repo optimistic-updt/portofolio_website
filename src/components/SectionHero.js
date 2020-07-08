35abdf87e2ece28b15dbe58cc2e4ba1280cfa2ad
Kevi G <kevgarciamusic@hotmail.com>
2020-07-08 18:16:53 +1000

    navbar out

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import './SectionHero.css'
 
-// import NavBar from './NavBar'
-
-// import {ReactComponent as MyLogo} from './KGF_logo.svg';
 import RubyLogo from './Ruby'
 import NodeLogo from './NodeLogo'
 import ReactLogo from './ReactLogo'
@@ -14,17 +11,12 @@ import Roll from 'react-reveal/Roll';
 
 
 
-
-
-
 function SectionHero() {
   return (
     <section id="home" className="section section-hero">
-    
-      {/* <NavBar /> */}
-
       <div className="main-wrapper span_8_of_12">
         <Fade bottom cascade big duration={1200}>
+        
         <div className="section-hero--text">
           <h1>Hey, I'm Kevin<span className="hand" role="img">👋</span></h1>
           <h2>Software Developer</h2>
@@ -36,6 +28,7 @@ function SectionHero() {
           </div>
         </div>
         </Fade>
+
         <Roll delay={1700} duration={1500}>
           <img src="/images/profile_photo.png" alt="Kevin drinking coffee" className="section-hero--hero-image"/>
         </Roll>
