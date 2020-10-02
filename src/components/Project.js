d838bbcf5d49480a47b20eac7ddfc4ed892b5848
Kevi G <kevgarciamusic@hotmail.com>
2020-10-02 16:01:48 +1000

    playing with react motion

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,43 +1,45 @@
-import React from 'react'
-import './Project.css'
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import "./Project.css";
 
-class Project extends React.Component {
-  
-  state = {
-    isActive: false
-  }
+const calc = (x, y) => [
+  -(y - window.innerHeight / 8) / 20,
+  (x - window.innerWidth / 8) / 20,
+  1.1,
+];
 
-  activate = () => { this.setState({ isActive: true })}
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-  deActivate = () => { this.setState({ isActive: false })}
+const Project = ({ imgURL, link, title, description, keyword }) => {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
 
-  render() {
+  return (
+    <a href={link} target="blank">
+      <animated.div
+        className="project-square"
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{
+          transform: props.xys.interpolate(trans),
+          backgroundImage: `url(${imgURL})`,
+        }}
+      >
+        <div className="project-overlay">
+          <h6>{title}</h6>
 
-    let style = {
-      backgroundImage: `url(${this.props.imgURL})`
-    }
-    
-    return (
-      <a href={this.props.link} target="blank">
-        <div 
-          className="project-square" 
-          style={style} 
-          onMouseEnter={this.activate} 
-          onMouseLeave={this.deActivate}>
-          
-          <div className="project-overlay">
-            <h6>{this.props.title}</h6>
- 
-              <div className="body-animation">
-                <p>{this.props.description}</p>
-                <p className="project-overlay--keyword">Keywords:</p>
-                <p>{this.props.keyword}</p>  
-              </div>
+          <div className="body-animation">
+            <p>{description}</p>
+            <p className="project-overlay--keyword">Keywords:</p>
+            <p>{keyword}</p>
           </div>
         </div>
-      </a>
-    )
-  }
-}
+      </animated.div>
+    </a>
+  );
+};
 
-export default Project
+export default Project;
