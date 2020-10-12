9996e7e3718fe201130aaecb39cb08669b3f967f
Kevi G <kevgarciamusic@hotmail.com>
2020-10-12 20:55:53 +1100

    content change

--- src/components/about_paragraphs/Values.js
+++ src/components/about_paragraphs/Values.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function Values() {
   return (
-      <ul className="values-list">
-        <li><a href="https://en.wikipedia.org/wiki/The_Alchemist_(novel)" target="blank">Go After Your Dreams</a></li>
-        <li>Make Something of Everyday</li>
-        <li>Have Fun</li>
-        <li>Do Cool Shit</li>
-        <li><a href="https://en.wikipedia.org/wiki/Pareto_principle" target="blank">80/20 to the Max</a></li>
-        <li>Work Hard</li>
-        <li>Live Free</li>
-      </ul>
-  )
+    <ul className="values-list">
+      <li>
+        <a
+          href="https://en.wikipedia.org/wiki/The_Alchemist_(novel)"
+          target="blank"
+        >
+          Go After Your Dreams
+        </a>
+      </li>
+      <li>Make Something of Everyday</li>
+      <li>Have Fun</li>
+      <li>
+        <a
+          href="https://www.youtube.com/watch?v=dylrDOcXEJ0&ab_channel=LoadedAmbassadors"
+          target="blank"
+        >
+          Do Cool Shit
+        </a>
+      </li>
+      <li>
+        <a href="https://en.wikipedia.org/wiki/Pareto_principle" target="blank">
+          80/20 to the Max
+        </a>
+      </li>
+      <li>Work Hard</li>
+      <li>Live Free</li>
+    </ul>
+  );
 }
 
-export default Values
+export default Values;
--- src/components/about_paragraphs/Who.js
+++ src/components/about_paragraphs/Who.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 // import '../SectionAbout.css'
 
-function Who() {
+const Who = () => {
   return (
-    <div>
+    <>
       <p>
-        I’m an aspiring <span className="full-stack">Full-Stack Software Developer</span> passionate about Ruby, Rails and front-end frameworks such as ReactJS, etc.
+        I’m a junior{" "}
+        <span className="full-stack">Front-end Software Developer</span>{" "}
+        passionate about ReactJS, NodeJS, Ruby/Rails and much more,
       </p>
       <br></br>
       <p>
-        I love teaming up with fun, hard-working people who are passionate about what they do, push themselves to grow and have big dreams.
+        I love teaming up with fun, hard-working people who are passionate about
+        what they do, push themselves to grow and have big dreams.
       </p>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Who
+export default Who;
